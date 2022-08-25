f18dfb04cfe55f36f471137548d14db83e89b653
francisyzy <francisyzy@me.com>
2022-08-25 12:13:07 +0800

    Try update DB during ping to prevent sleep

--- src/ping.ts
+++ src/ping.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import config from "./config";
 
 const prisma = new PrismaClient();
 
 const ping = () => {
   prisma.$connect;
   prisma.user.count;
+  prisma.user.update({
+    where: { telegramId: config.ADMIN_TELEGRAM_ID },
+    data: { updatedAt: new Date() },
+  });
   prisma.$disconnect;
 };
 export default ping;
